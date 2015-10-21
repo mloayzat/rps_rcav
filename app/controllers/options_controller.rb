af7662a2e0b50febaffa434a37c666f8ebcee5be
mloayzat <mloayzat@chicagobooth.edu>
2015-10-21 16:01:34 -0500

    done

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -35,13 +35,67 @@ class OptionsController < ApplicationController
 	end
 
 	def paper
-		@move = ["Rock", "Paper", "Scissors"].sample 
+			@move = ["Rock", "Paper", "Scissors"].sample 
+
+	@answer =	if "Paper" == @move 
+    @answer = "You tied!"
+        elsif "Scissors" == @move  
+    @answer= "You lost!"
+        elsif "Rock" == @move
+    @answer = "You won!"
+
+    	end
+
+
+	@result_image =	if "Paper" == @move 
+    @result_image = "paper"
+        			elsif "Scissors" == @move  
+    @result_image= "scissors"
+       				elsif "Rock" == @move
+    @result_image = "rock"
+    	end
+
+    @label_color =	if "Paper" == @move 
+    @label_color = "warning"
+        			elsif "Scissors" == @move  
+    @label_color= "danger"
+       				elsif "Rock" == @move
+    @label_color = "success"
+    	end
+ 
 		render ("paper.html.erb")
 
 	end
 
 	def scissors
-		@move = ["Rock", "Paper", "Scissors"].sample 
+			@move = ["Rock", "Paper", "Scissors"].sample 
+
+	@answer =	if "Scissors" == @move 
+    @answer = "You tied!"
+        elsif "Rock" == @move  
+    @answer= "You lost!"
+        elsif "Paper" == @move
+    @answer = "You won!"
+
+    	end
+
+
+	@result_image =	if "Rock" == @move 
+    @result_image = "rock"
+        			elsif "Paper" == @move  
+    @result_image= "paper"
+       				elsif "Scissors" == @move
+    @result_image = "scissors"
+    	end
+
+    @label_color =	if "Scissors" == @move 
+    @label_color = "warning"
+        			elsif "Rock" == @move  
+    @label_color= "danger"
+       				elsif "Paper" == @move
+    @label_color = "success"
+    	end
+
 		render ("scissors.html.erb")
 
 	end
